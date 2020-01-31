4904fb764ff5a3c89bdfbaefeb061f6cbf9a94c3
Luis Magalhaes <sprintlmaga@gmail.com>
2020-01-31 17:19:52 +0000

    foreach element of <li> implemented to download zip attachment files.

--- SeleniumTech.cs
+++ SeleniumTech.cs
@@ -13,35 +13,25 @@ namespace ZipWork
             
             using (var driver = new OpenQA.Selenium.Chrome.ChromeDriver())
             {
-                UrlPath = "https://classifiedwebpath/" + UrlPath;
+                UrlPath = "confidentialPath/" + UrlPath;
 
                 driver.Navigate().GoToUrl(UrlPath);
 
-                System.Threading.Thread.Sleep(5000);
-                
-                //var WebElemAttach = driver.FindElement(OpenQA.Selenium.By.ClassName("item-attachments"));
+                System.Threading.Thread.Sleep(15000);
 
                 IWebElement PageElements = driver.FindElement(By.ClassName("item-attachments"));
                 var myAttach = PageElements.FindElements(By.TagName("li"));
                 foreach (var item in myAttach)
                 {
                     Console.WriteLine(item.Text);
-                    item.Click();
-                    Console.WriteLine("Downloading File...");
+                    Console.WriteLine(item.GetAttribute("data-downloadurl").ToString());
+                    if (item.GetAttribute("data-downloadurl").ToString().StartsWith("application/zip:"))
+                    {
+                        item.Click();
+                        Console.WriteLine("Downloading File...");
+                    }
                     System.Threading.Thread.Sleep(2000);
                 }
-
-                System.Threading.Thread.Sleep(500);
-                var searchDiv = driver.FindElement(OpenQA.Selenium.By.Id("search"));
-                var firstlink = searchDiv.FindElement(OpenQA.Selenium.By.TagName("a"));
-                firstlink.Click();
-
-
-    
-                System.Threading.Thread.Sleep(10000);
-                
-
-
             }
         }
 
